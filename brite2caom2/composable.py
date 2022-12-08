2601a76beb08b7818aac69df6dc8a22644c16d22
Sharon Goliath <Sharon.Goliath@nrc-cnrc.gc.ca>
2022-12-08 09:49:51 -0800

    CADC-10715 - fix a spelling mistake.

--- brite2caom2/composable.py
+++ brite2caom2/composable.py
@@ -89,7 +89,7 @@ from caom2pipe.transfer_composable import modify_transfer_factory, store_transfe
 from brite2caom2 import data_source, main_app, reader, storage_name
 from brite2caom2 import fits2caom2_augmentation, preview_augmentation
 
-BRITE_BOOKMARK = 'brite_timestmap'
+BRITE_BOOKMARK = 'brite_timestamp'
 META_VISITORS = [fits2caom2_augmentation, preview_augmentation]
 DATA_VISITORS = []
 
